246b9e802227cc9182775a1faa088470a97c5e50
Linner, David <david.linner@wolterskluwer.com>
2018-02-23 12:14:43 +0100

    Draft version of bucketize

---
+++ src/bucketize.spec.ts
@@ -0,0 +1,105 @@
+import { bucketize } from './index';
+
+describe("bucketize", () => {
+
+    it("equal length", () => {
+
+        let black = [
+            { id: "a", value: 10, weight: 1 },
+            { id: "b", value: 10, weight: 0 }
+        ];
+
+        let white = [
+            { id: "a", value: 5, weight: 0 },
+            { id: "b", value: 5, weight: 1 }
+        ];
+
+        let blackWins = [
+            { id: "a", value: 10}
+        ];
+
+        let whiteWins = [
+            { id: "b", value: 5}
+        ];
+
+        let buckets = bucketize({black, white}, {black: blackWins, white: whiteWins});
+
+        expect(buckets).toHaveLength(1);
+
+        let firstBucket = buckets[0];
+
+        expect(firstBucket.black).toContainEqual({id: "a", value: 10});
+        expect(firstBucket.white).toContainEqual({id: "b", value: 5});
+    });
+
+
+    it("different length", () => {
+
+        let black = [
+            { id: "a", value: 10, weight: .75 },
+            { id: "b", value: 10, weight: .75 }
+        ];
+
+        let white = [
+            { id: "a", value: 5, weight: .5 },
+            { id: "b", value: 5, weight: .5 }
+        ];
+
+        let blackWins = [
+            { id: "a", value: 10},
+            { id: "b", value: 10}
+        ];
+
+        let whiteWins = [
+        ];
+
+        let buckets = bucketize({black, white}, {black: blackWins, white: whiteWins});
+
+        expect(buckets).toHaveLength(1);
+
+        let firstBucket = buckets[0];
+
+        expect(firstBucket.black).toEqual(expect.arrayContaining([{id: "a", value: 10}, {id:"b", value: 5}]));
+
+        expect(firstBucket.white).toHaveLength(0);
+    });
+
+    it("balance bucket on weight", () => {
+
+        let black = [
+            { id: "a", value: 10, weight: .9 },
+            { id: "b", value: 10, weight: 0 },
+            { id: "c", value: 10, weight: 0 },
+            { id: "d", value: 10, weight: 0.1 }
+        ];
+
+        let white = [
+            { id: "a", value: 5, weight: 0.5 },
+            { id: "b", value: 5, weight: 0.4 },
+            { id: "c", value: 5, weight: 0.1 },
+            { id: "d", value: 5, weight: 0 }
+        ];
+
+        let blackWins = [
+            { id: "a", value: 10},
+            { id: "d", value: 10}
+        ];
+
+        let whiteWins = [
+            { id: "b", value: 5},
+            { id: "c", value: 5}
+        ];
+
+        let buckets = bucketize({black, white}, {black: blackWins, white: whiteWins});
+
+        expect(buckets).toHaveLength(2);
+
+        let firstBucket = buckets[0];
+
+        expect(firstBucket.black).toContainEqual({id: "a", value: 10});
+
+        expect(firstBucket.white).toEqual(expect.arrayContaining([{id: "b", value: 5}, {id: "c", value: 5}]));
+    });
+
+
+});
--- src/index.ts
+++ src/index.ts
@@ -22,7 +22,7 @@ export function propose(initial: Position, consensus?: Consensus): Consensus{
     }
 
     let terms  = _.map(white, (v1) => {
-        let v2 = _.find(black, (v) => v.id = v1.id);
+        let v2 = _.find(black, (v) => v.id == v1.id);
         return {
             id: v1.id,
             black : {
@@ -97,6 +97,77 @@ export function propose(initial: Position, consensus?: Consensus): Consensus{
     return results;
 }
 
-// export function bucketize(initial: Position, proposal: Consensus) {
-//     //return ;
-// }
+export function bucketize(initial: Position, proposal: Consensus): Consensus[] {
+
+    // todo: strip euqal positions
+
+    let initialBlack = initial.black;
+    let initialWhite = initial.white;
+
+    let black = _
+        .chain(proposal.black)
+        .map((item) => {
+            let iB = _.find(initialBlack, (b)=>b.id == item.id);
+            return _.merge(item, { weight: iB.weight});
+        })
+        .sortBy((item) => -item.weight)
+        .value();
+
+    let white = _
+        .chain(proposal.white)
+        .map((item) => {
+            let iB = _.find(initialWhite, (b)=>b.id == item.id);
+            return _.merge(item, { weight: iB.weight});
+        })
+        .sortBy((item) => -item.weight)
+        .value();
+
+    let result: Consensus[] = [];
+
+    let bucket = {
+        black : [],
+        white : []
+    };
+
+    while (!_.isEmpty(white) || !_.isEmpty(black)) {
+
+        if (_.isEmpty(white)) {
+            bucket.black = _.chain(black).map((item) => _.omit(item, ['weight'])).value();
+            black = [];
+        } else if (_.isEmpty(black)) {
+            bucket.white = _.chain(white).map((item) => _.omit(item, ['weight'])).value();
+            white = [];
+        } else {
+            let ww = white[0].weight;
+            let bw = black[0].weight;
+
+            bucket.white.push(_.omit(white.shift(), ['weight']));
+            bucket.black.push(_.omit(black.shift(), ['weight']));
+
+            if(ww > bw){
+                do {
+                    if(_.isEmpty(black)) break;
+                    let x = black.shift();
+                    bucket.black.push(_.omit(x, ['weight']));
+                    bw += x.weight;
+                } while (ww > bw);
+            } else if (ww < bw) {
+                do {
+                    if(_.isEmpty(white)) break;
+                    let x = white.shift();
+                    bucket.white.push(_.omit(x, ['weight']));
+                    ww += x.weight;
+                } while (ww < bw);
+            }
+        }
+
+        result.push(bucket);
+
+        bucket = {
+            black : [],
+            white : []
+        };
+    }
+
+    return result;
+}
